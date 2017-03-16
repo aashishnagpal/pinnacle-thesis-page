eb370718cf90b3de3ff33591d41445758cc5eb35
aashishnagpal <aashish.nagpal@hotmail.com>
2017-03-16 14:24:12 +0530

    Added Team member list styles and images + Updated a few other styles and location of SCSS files

--- app/js/app.js
+++ app/js/app.js
@@ -18,28 +18,72 @@
     "pinnacleProject": {
       "fullName": "Vojislav Grujić",
       "careerPathsAndGradDates": [
-        "Advanced Frontend Developer,  July 2017",
-        "Advanced Fullstack Developer,  December 2017"
+        "Advanced Frontend Developer, July 2017",
+        "Advanced Fullstack Developer, December 2017"
       ],
-
       "title": "The Great Pineapple Project",
       "featuredImage": "http://placehold.it/1200x500",
-
       "description": "Denounce your PLEASURE and Embrace your PAIN!!!",
       "problemSolving": "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. \nNor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?",
       "solution": "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. \nBut in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.",
       "humanityImpact": "But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure? On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. \nThese cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided.\n\nBut in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains. But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.\n\nThese cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains. But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. \nNo one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.",
-
       "teamProject": "yes",
       "team": [
-        "no",
-        "body",
-        "with",
-        "me",
-        "I'm",
-        "alone"
+        {
+          "name": "Jonas Lininger",
+          "image": "https://images.pexels.com/photos/101584/pexels-photo-101584.jpeg?w=470&h=200&auto=compress&cs=tinysrgb",
+          "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a semper sem. Ut pellentesque libero ut ultricies vehicula. Morbi suscipit felis nulla, luctus ornare nunc fringilla et",
+          "twitter": "#",
+          "fb": "#",
+          "gh": "#",
+          "linkedin": "#"
+        },
+        {
+          "name": "Mariam Casaus",
+          "image": "https://images.pexels.com/photos/27411/pexels-photo-27411.jpg?w=470&h=200&auto=compress&cs=tinysrgb",
+          "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a semper sem. Ut pellentesque libero ut ultricies vehicula. Morbi suscipit felis nulla, luctus ornare nunc fringilla et",
+          "twitter": "#",
+          "fb": "#",
+          "gh": "#",
+          "linkedin": "#"
+        },
+        {
+          "name": "Herbert Stamper",
+          "image": "https://images.pexels.com/photos/119705/pexels-photo-119705.jpeg?w=470&h=200&auto=compress&cs=tinysrgb",
+          "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a semper sem. Ut pellentesque libero ut ultricies vehicula. Morbi suscipit felis nulla, luctus ornare nunc fringilla et",
+          "twitter": "#",
+          "fb": "#",
+          "gh": "#",
+          "linkedin": "#"
+        },
+        {
+          "name": "Alyssa Mcglothin",
+          "image": "https://images.pexels.com/photos/206559/pexels-photo-206559.jpeg?w=470&h=200&auto=compress&cs=tinysrgb",
+          "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a semper sem. Ut pellentesque libero ut ultricies vehicula. Morbi suscipit felis nulla, luctus ornare nunc fringilla et",
+          "twitter": "#",
+          "fb": "#",
+          "gh": "#",
+          "linkedin": "#"
+        },
+        {
+          "name": "Claudio Cunha",
+          "image": "https://images.pexels.com/photos/211050/pexels-photo-211050.jpeg?w=470&h=200&auto=compress&cs=tinysrgb",
+          "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a semper sem. Ut pellentesque libero ut ultricies vehicula. Morbi suscipit felis nulla, luctus ornare nunc fringilla et",
+          "twitter": "#",
+          "fb": "#",
+          "gh": "#",
+          "linkedin": "#"
+        },
+        {
+          "name": "Elvera Stacks",
+          "image": "https://images.pexels.com/photos/27949/pexels-photo-27949.jpg?w=470&h=200&auto=compress&cs=tinysrgb",
+          "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a semper sem. Ut pellentesque libero ut ultricies vehicula. Morbi suscipit felis nulla, luctus ornare nunc fringilla et",
+          "twitter": "#",
+          "fb": "#",
+          "gh": "#",
+          "linkedin": "#"
+        }
       ],
-
       "thesisApproved": "yes",
       "completionDate": "",
       "link": "http://here.and.there.bla",
